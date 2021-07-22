efe2064268db2c9e38b5f4008554847af3fccc38
Jacob Molsby <jacobmolsby@hotmail.se>
2021-07-22 20:08:43 +0000

    crud complete

--- todo/models.py
+++ todo/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
 # Create your models here.
-# When creating this item, django will automatically create a table for this class in the database.
+# When creating this item, django will automatically create a
+#  table for this class in the database.
+
+
 class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
+
+    # overrides the str method of how our items are displaced,
+    # str method is defines in django.db
+    def __str__(self):
+        return self.name
